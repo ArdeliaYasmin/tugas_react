4fa3d83bf939e6ee85a7ce86b0923f516acf7c2f
Ardelia Yasmin <ardeliascarlet@gmail.com>
2023-05-14 01:09:14 +0700

    add visimisi

---
+++ src/components/VisiMisi.js
@@ -0,0 +1,26 @@
+function VisiMisi() {
+    return (
+      <>
+        <div style={{marginTop:'15rem',width:'100%',height:'10px'}} className="about-scroll"></div>
+  
+        <div className="container visimisi">
+          <div className="row">
+            <div className="col-md-6 text-justify">
+            <h2 className="main-title about-h2">VISI & MISI</h2>
+              <p className="main-p">
+              Menjadi perusahaan terbaik dibidang industri converting carton
+              box yang menghasilkan produk dan jasa pelayanan terbaik bagi
+              kepuasan pelanggan 
+              </p>
+            </div>
+            <div className="col-md-6 text-center">
+            <img alt="about" src="./img/img1.png" className="img-fluid" />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+  export default VisiMisi;
+  
+
